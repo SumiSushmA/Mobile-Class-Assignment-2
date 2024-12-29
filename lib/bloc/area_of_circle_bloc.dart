e79fb6a2c4427aea0a6bdec2155fbb8c370a0e16
Sushma Sharma <154212342+SumiSushmA@users.noreply.github.com>
2024-12-29 15:10:07 +0545

    Area of Circle

---
+++ lib/bloc/area_of_circle_bloc.dart
@@ -0,0 +1,14 @@
+// area_of_circle_bloc.dart
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'area_of_circle_event.dart';
+
+class AreaOfCircleBloc extends Bloc<AreaOfCircleEvent, double> {
+  AreaOfCircleBloc() : super(0.0) {
+    on<CalculateAreaEvent>((event, emit) {
+      final area = 3.14159 * event.radius * event.radius;
+      emit(area);
+    });
+  }
+}
